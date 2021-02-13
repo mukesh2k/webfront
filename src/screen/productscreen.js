b5bf976a8956b875a6927182616e0e91794b5913
Mukesh <mukesh192k@gmail.com>
2021-02-13 22:12:05 +0530

    changes have been made

--- src/screen/productscreen.js
+++ src/screen/productscreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Rating from "../components/Rating";
 import data from "../data";
 export default function productscreen(props) {
@@ -12,7 +13,8 @@ export default function productscreen(props) {
     );
   }
   return (
-    <div className="row">
+    <div className="row top">
+      <Link to="/">Back to results</Link>
       <div className="col-2">
         <img src={product.img} alt={product.name} />
       </div>
@@ -31,10 +33,28 @@ export default function productscreen(props) {
         </ul>
       </div>
       <div className="col-1">
-        <div className="card card-body">
+        <div className="card card-bd">
           <ul>
             <li>
-              <div className="row"></div>
+              <div className="row">
+                <div>Price </div>
+                <div className="price">{product.price}</div>
+              </div>
+            </li>
+            <li>
+              <div className="row">
+                <div>Status :</div>
+                <div>
+                  {product.noofstock > 0 ? (
+                    <span className="success">In Stock</span>
+                  ) : (
+                    <span className="error">Out of Stock</span>
+                  )}
+                </div>
+              </div>
+            </li>
+            <li>
+              <button className="primary block">Add to cart</button>
             </li>
           </ul>
         </div>
